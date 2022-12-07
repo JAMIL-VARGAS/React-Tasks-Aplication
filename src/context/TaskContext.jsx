9588160fd381a6831d4adb588cee23b6db19d6bd
JAMIL-VARGAS <vargasjamil@gmail.com>
2022-12-07 18:37:54 -0500

    Primer commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,40 @@
+import {createContext,useEffect,useState} from 'react'
+import {tasks as data} from '../data/tasks'
+
+export const TaskContext = createContext()
+
+export function TaskContextProvider(props) {
+    // Atajo useStateSnippet
+    // CREAMOS UN STATE PARA MODIFICAR EL ESTADO DE LAS TAREAS
+    const [tasks, setTasks] = useState([]);
+    
+
+    // EL ESTADO SE LLENARA CON EL ARRAY tasks osea data al cargar el componente
+    useEffect(() => {
+        setTasks(data)
+    },[])
+
+    // ESTA FUNCION HACE UN COPIA DEL ARREGLO Y LE AGREGA LA NUEVA TAREA CON SUS RESPECTIVOS VALORES
+    function createTask(taskTitle,taskDescription){
+      setTasks([...tasks,{
+        title: taskTitle,
+        // EL ID INCREMENTA CONFORME SE VAYA AGREGANDO
+        id: tasks.length,
+        description: taskDescription
+      }])
+    }
+
+    function deleteTask(taskId){
+      setTasks(tasks.filter( task => task.id != taskId))
+    }
+    
+    return (
+        <TaskContext.Provider value={{
+            tasks,
+            createTask,
+            deleteTask
+        }}>
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
