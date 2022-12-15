5c78ffe85f8482a5ff127f0bf105a6e7abcd8e77
JAMIL-VARGAS <vargasjamil@gmail.com>
2022-12-15 02:06:09 -0500

    Segundo commit

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,7 +7,7 @@ function TaskList() {
     const {tasks} = useContext(TaskContext)
     // VALIDAMOS QUE EXISTAN TAREAS DENTRO DEL ARRAY
     if(tasks.length === 0){
-        return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aún</h1>
+        return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aún...</h1>
     }
     return (
         <div className = "grid grid-cols-4 gap-2">
